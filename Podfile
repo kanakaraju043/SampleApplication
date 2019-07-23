2b09f06b9d38f1a23cd6b46270fc0fb7d3381d53
kanakaraju <kanakaraju.gandreddi@gmail.com>
2019-07-23 17:32:26 +0530

    commit

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '12.2'
+use_frameworks!
+
+target 'SampleApplication' do
+  pod 'AFNetworking', '~> 2.6'
+end
---
+++ SampleApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApplication
+//
+//  Created by kanakaraju on 23/07/19.
+//  Copyright © 2019 Administrator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
